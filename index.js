bb34e8cec6879a5fcd5c43c7446938ca68ef926a
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-06-15 22:41:31 -0500

    trying to add table of contents

--- index.js
+++ index.js
@@ -8,26 +8,36 @@ const questions = [
 {
     type: "input",
     name: "title",
-    message: "What is your name?",
-    default: "Ramon"
+    message: "What is the title of your project",
+    //default: "Ramon"
 },
 {
     type: "input",
-    name: "location",
-    message: "Where are you from?",
-    default: "Houston"
+    name: "description",
+    message: "Describe ReadMe",
+    //default: "Houston"
   },
   {
     type: "input",
-    name: "color",
-    message: "What is your favorite color?",
-    default: "red"
+    name: "Table of Contents",
+    message: "Table of Contents",
+    default: "I II III IV V"
   },
   {
     type: "list",
     name: "license",
     message: "What kind of license should your project have?",
     choices: ["MIT", "APACHE", "GPL", "BSD", "None"]
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: "List all Contributors",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Describe Usage",
   },
 ];
 
